28a225f0ce20a0ebc198848492c2273446b22d55
Vadim Smakhtin <vadim.smakhtin@mathrioshka.ru>
2019-12-15 23:15:07 +0300

    Added map with markers

--- src/screens/Map/MapScreen.tsx
+++ src/screens/Map/MapScreen.tsx
@@ -1,11 +1,79 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Dimensions } from "react-native";
 import { Text } from "react-native-paper";
+import MapView, { Marker, LatLng, Region, Callout } from "react-native-maps";
+import { Location, Permissions, Constants } from "expo";
+
+const testMarkers = [
+  {
+    id: "0",
+    coordinate: { latitude: 37.763056, longitude: -122.4463657 },
+    title: "test0",
+    description: "This is test 0 marker"
+  },
+  {
+    id: "1",
+    coordinate: { latitude: 37.7663521, longitude: -122.4443481 },
+    title: "test1",
+    description: "This is test 1 marker"
+  }
+];
+
+const testInitialRegion = {
+  latitude: 37.78825,
+  longitude: -122.4324,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421
+};
+
+type MapMarker = {
+  id: string;
+  coordinate: LatLng;
+  title: string;
+  description?: string;
+};
 
 export const MapScreen: React.FC = () => {
+  const [region, setRegion] = useState<Region>(testInitialRegion);
+
+  const [markers, setMarkers] = useState<Array<MapMarker>>(testMarkers);
+
+  const onRegionChange = (newRegion: Region) => {
+    setRegion(newRegion);
+  };
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text> This is my Map screen </Text>
+    <View style={styles.container}>
+      <MapView
+        style={styles.mapStyle}
+        initialRegion={region}
+        onRegionChange={onRegionChange}
+      >
+        {markers.map(marker => (
+          <Marker
+            coordinate={marker.coordinate}
+            title={marker.title}
+            key={marker.id}
+          >
+            <Callout>
+              <Text>{marker.title}</Text>
+            </Callout>
+          </Marker>
+        ))}
+      </MapView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  mapStyle: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height
+  }
+});
