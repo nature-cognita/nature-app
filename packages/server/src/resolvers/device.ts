cd2861efe46b312abe0c804d4a8e047a8c410342
Vadim Smakhtin <vadim.smakhtin@mathrioshka.ru>
2020-01-05 11:57:29 +0300

    Added sensors resolver

--- packages/server/src/resolvers/device.ts
+++ packages/server/src/resolvers/device.ts
@@ -1,10 +1,13 @@
-import { QueryResolvers, Device, DeviceResolvers } from "generated/types";
+import {
+  QueryResolvers,
+  Device,
+  DeviceResolvers,
+  Sensor
+} from "generated/types";
 
 export const deviceQueries: QueryResolvers = {
   devices: async (_parent, _args, { prisma }) => {
     const devices = (await prisma.devices()) as Array<Device>;
-
-    console.log(devices);
 
     return devices;
   }
@@ -13,5 +16,11 @@ export const deviceQueries: QueryResolvers = {
 export const deviceResolvers: DeviceResolvers = {
   location(parent, _args, { prisma }) {
     return prisma.device({ id: parent.id }).location();
+  },
+
+  sensors(parent, _args, { prisma }) {
+    return (prisma.sensors({
+      where: { device: { id: parent.id } }
+    }) as unknown) as Array<Sensor>; //TODO: This typecasting looks strange (prisma vs gqlgen)
   }
 };
