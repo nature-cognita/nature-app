b8f8ed12de8317157a3a7380ff899e2142ed5b54
Vadim Smakhtin <vadim.smakhtin@habidatum.com>
2020-04-01 21:06:52 +0300

    Can sync data from plant

--- packages/app/src/screens/Plant/PlantScreen.tsx
+++ packages/app/src/screens/Plant/PlantScreen.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { View } from "react-native";
 import { Text } from "react-native-paper";
+import { Badge, Button } from "react-native-paper";
+
+const PLANT_URL = "http://localhost:3000";
 
 export const PlantScreen: React.FC = () => {
+  const loadPlantData = () => {
+    fetch(`${PLANT_URL}`)
+      .then(responce => responce.json())
+      .then(responceJson => console.log(responceJson));
+  };
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text> This is my Plant screen </Text>
+      <Button icon="sync" mode="contained" onPress={loadPlantData}>
+        Load Plant Data
+      </Button>
     </View>
   );
 };
