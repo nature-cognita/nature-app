fb0d2a50b84bae0aeba37a14c6f28a5e9b00dea6
Vadim Smakhtin <vadim.smakhtin@habidatum.com>
2021-04-02 20:03:17 +0300

    Spheres working

--- packages/app/src/screens/Plant/PlantScreen.tsx
+++ packages/app/src/screens/Plant/PlantScreen.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState, useMemo, useRef } from "react";
 import { View } from "react-native";
 import { Button, Text } from "react-native-paper";
 import * as SQLite from "expo-sqlite";
@@ -6,6 +6,7 @@ import { DatabaseContext, recordsCountAtom } from "../../store";
 import { useContext } from "react";
 import { useAtom } from "jotai";
 import { Canvas, useFrame } from "react-three-fiber";
+import * as THREE from "three";
 
 const PLANT_URL = "http://localhost:4000";
 
@@ -23,6 +24,71 @@ const Box = () => {
     </mesh>
   );
 };
+
+function Swarm({ count }) {
+  const mesh = useRef(null);
+  const [dummy] = useState(() => new THREE.Object3D());
+
+  const particles = useMemo(() => {
+    const temp = [];
+    for (let i = 0; i < count; i++) {
+      const t = Math.random() * 100;
+      const factor = 20 + Math.random() * 100;
+      const speed = 0.01 + Math.random() / 200;
+      const xFactor = -20 + Math.random() * 40;
+      const yFactor = -20 + Math.random() * 40;
+      const zFactor = -20 + Math.random() * 40;
+      temp.push({ t, factor, speed, xFactor, yFactor, zFactor, mx: 0, my: 0 });
+    }
+    return temp;
+  }, [count]);
+
+  useFrame((state) => {
+    particles.forEach((particle, i) => {
+      let { t, factor, speed, xFactor, yFactor, zFactor } = particle;
+      t = particle.t += speed / 2;
+      const a = Math.cos(t) + Math.sin(t * 1) / 10;
+      const b = Math.sin(t) + Math.cos(t * 2) / 10;
+      const s = Math.max(1.5, Math.cos(t) * 5);
+      particle.mx +=
+        (state.mouse.x * state.viewport.width - particle.mx) * 0.02;
+      particle.my +=
+        (state.mouse.y * state.viewport.height - particle.my) * 0.02;
+      dummy.position.set(
+        (particle.mx / 10) * a +
+          xFactor +
+          Math.cos((t / 10) * factor) +
+          (Math.sin(t * 1) * factor) / 10,
+        (particle.my / 10) * b +
+          yFactor +
+          Math.sin((t / 10) * factor) +
+          (Math.cos(t * 2) * factor) / 10,
+        (particle.my / 10) * b +
+          zFactor +
+          Math.cos((t / 10) * factor) +
+          (Math.sin(t * 3) * factor) / 10
+      );
+      dummy.scale.set(s, s, s);
+      dummy.updateMatrix();
+      mesh.current.setMatrixAt(i, dummy.matrix);
+    });
+    mesh.current.instanceMatrix.needsUpdate = true;
+  });
+
+  return (
+    <>
+      <instancedMesh
+        ref={mesh}
+        args={[null, null, count]}
+        castShadow
+        receiveShadow
+      >
+        <sphereBufferGeometry args={[1, 32, 32]} />
+        <meshPhongMaterial />
+      </instancedMesh>
+    </>
+  );
+}
 
 export const PlantScreen: React.FC = () => {
   const [recordsCount, setRecordsCount] = useAtom(recordsCountAtom);
@@ -84,12 +150,17 @@ export const PlantScreen: React.FC = () => {
           width: "100%",
           height: "100%",
         }}
+        camera={{ fov: 75, position: [0, 0, 70], near: 10, far: 150 }}
+        shadowMap
+        gl={{ alpha: false, antialias: false }}
       >
+        <ambientLight intensity={1.5} />
+        <pointLight position={[100, 100, 100]} intensity={2} castShadow />
+        <pointLight position={[-100, -100, -100]} intensity={5} color="red" />
         <Suspense fallback={null}>
-          <ambientLight />
-          <pointLight position={[-1, 1, 1]} castShadow />
           <Box />
         </Suspense>
+        <Swarm count={150} />
       </Canvas>
       <Text>Stored Records Count: {recordsCount} </Text>
       <Button icon="download" mode="contained" onPress={downloadPlantData}>
