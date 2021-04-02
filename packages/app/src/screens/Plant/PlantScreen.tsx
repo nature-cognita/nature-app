8ec27fa9485aaef8f59a6ad39d910d797b4d90a0
Vadim Smakhtin <vadim.smakhtin@habidatum.com>
2021-04-02 19:29:13 +0300

    Gl scene works

--- packages/app/src/screens/Plant/PlantScreen.tsx
+++ packages/app/src/screens/Plant/PlantScreen.tsx
@@ -1,11 +1,28 @@
-import React, { useState } from "react";
+import React, { Suspense } from "react";
 import { View } from "react-native";
 import { Button, Text } from "react-native-paper";
 import * as SQLite from "expo-sqlite";
 import { DatabaseContext, recordsCountAtom } from "../../store";
 import { useContext } from "react";
 import { useAtom } from "jotai";
+import { Canvas, useFrame } from "react-three-fiber";
+
 const PLANT_URL = "http://localhost:4000";
+
+const Box = () => {
+  useFrame(({ scene }) => {
+    const box = scene.getObjectByName("box");
+    box.rotation.x += 0.01;
+    box.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh name="box">
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshPhysicalMaterial color="blue" />
+    </mesh>
+  );
+};
 
 export const PlantScreen: React.FC = () => {
   const [recordsCount, setRecordsCount] = useAtom(recordsCountAtom);
@@ -59,6 +76,21 @@ export const PlantScreen: React.FC = () => {
 
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Canvas
+        style={{
+          position: "absolute",
+          top: 0,
+          bottom: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <Suspense fallback={null}>
+          <ambientLight />
+          <pointLight position={[-1, 1, 1]} castShadow />
+          <Box />
+        </Suspense>
+      </Canvas>
       <Text>Stored Records Count: {recordsCount} </Text>
       <Button icon="download" mode="contained" onPress={downloadPlantData}>
         Download plant data
